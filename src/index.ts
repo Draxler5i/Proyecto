1f96727b19ea2496b2f2b0037eb6e818eebce57b
Walter Rojas <walter.rojas@bolivia.assuresoft.com>
2022-10-24 18:37:34 -0400

    Conexion DB y request

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-// import express, { response } from 'express';
-
 import { response } from "express";
 
 const express = require('express')
@@ -7,7 +5,7 @@ const conexion = require('mysql')
 
 // Initializations
 const app = express();
-
+require('dotenv').config();
 const config = {
     host: 'localhost',
     user: 'root',
@@ -17,10 +15,11 @@ const config = {
 
 const pool = conexion.createPool(config);
 const users: string[] = [];
-
+const port = process.env.PORT 
+// 
 // settings
+// app.set('port', process.env.port || 4000);
 
-app.set('port', process.env.port || 4000);
 
 // middlewares
 app.use(express.json());
@@ -29,20 +28,20 @@ module.exports = pool;
 // Routes
 app.get('/users', (req: any, res: any) => {
 
-    // pool.query('SELECT * FROM apiTest', (error: any, result: any) => {
-    //     if (error) throw error;
-    //     res.send(result)
-    // });
+    pool.query('SELECT * FROM users', (error: any, result: any) => {
+        if (error) throw error;
+        res.send(result)
+    });
 
     res.json(users);
 });
 
 app.post('/users', (req: any, res: any) => {
 
-    // pool.query('INSERT INTO users SET ?', req.body, (error: any, result: any) => {
-    //     if (error) throw error;
-    //     res.status(201).send(`User added with ID: ${result.insertId}`);
-    // })
+    pool.query('INSERT INTO users SET ?', req.body, (error: any, result: any) => {
+        if (error) throw error;
+        res.status(201).send(`User added with ID: ${result.insertId}`);
+    })
 
     const user = req.body;
     users.push(user);
@@ -51,11 +50,11 @@ app.post('/users', (req: any, res: any) => {
 
 app.put('/users/:id', (req: any, res: any) => {
 
-    // const id = res.params.id;
-    //     pool.query('UPDATE users SET ? WHERE id = ?', [req.body, id], (error: any, result: any) => {
-    //       if (error) throw error;
-    //        res.send('User updated successfully.');
-    //   });
+    const id = res.params.id;
+        pool.query('UPDATE users SET ? WHERE id = ?', [req.body, id], (error: any, result: any) => {
+          if (error) throw error;
+           res.send('User updated successfully.');
+      });
 
     const user = req.body;
     users.push(user);
@@ -63,19 +62,22 @@ app.put('/users/:id', (req: any, res: any) => {
 });
 app.delete('/users/:id', (req: any, res: any) => {
 
-    // const id = res.params.id;
-    // pool.query('DELETE FROM users WHERE id =?', id, (error: any, result: any) => {
-    //     if (error) throw error;
-    //     res.send('User delete sucessfully')
-    // })
-
-
+    const id = res.params.id;
+    pool.query('DELETE FROM users WHERE id =?', id, (error: any, result: any) => {
+        if (error) throw error;
+        res.send('User delete sucessfully')
+    })
 
     const userDeleted = users.pop();
     res.json(userDeleted);
 });
 
 // Starting the Server
-app.listen(app.get('port'), () => {
-    console.log(`Server on port`, app.get('port'));
+
+// app.listen(app.get('port'), () => {
+//     console.log(`Server on port`, app.get('port'));
+// });
+
+app.listen(port, () => {
+    console.log(`Server on port`, port);
 });
