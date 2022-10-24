9af38ede2f903e5582843ccb0742499c87092130
DanieMerida <daniemerida@ascbominimac89.local>
2022-10-24 09:09:33 -0400

    Basic server Running

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express, { response } from 'express';
+
+// Initializations
+const app = express();
+
+const users: any = [];
+
+// settings
+app.set('port', 4000);
+
+// middlewares
+app.use(express.json());
+
+// Routes
+app.get('/users', (req: any, res: any) => {
+    res.json(users);
+});
+app.post('/users', (req: any, res: any) => {
+    const user = req.body;
+    users.push(user);
+    res.json(user);
+});
+app.put('/users/:id', (req: any, res: any) => {
+    const user = req.body;
+    users.push(user);
+    res.json(user);
+});
+app.delete('/users/:id', (req: any, res: any) => {
+    const userDeleted = users.pop();
+    res.json(userDeleted);
+
+});
+
+// Starting the Server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port`, app.get('port'));
+});
