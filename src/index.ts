0c946741a675cdb322948d0c58f1a7f2219a26ca
sharon calcina <sharon46calcina@gmail.com>
2022-10-26 17:06:47 -0600

    semicolons removed

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,21 @@
 
 import 'dotenv/config'
 const express = require('express')
-
 // Initializations
 const userServices = require('./services/userServices')
 const ticketServices = require('./services/ticketServices')
-
-const app = express();
+const app = express()
 const port = process.env.PORT || 4005
-app.use(express.json());
-// middlewares
+app.use(express.json())
+//Endpoints
 app.get('/users', userServices.getUser)
 app.post('/users', userServices.postUser)
 app.put('/users/:id', userServices.putUser)
 app.delete('/users/:id', userServices.deleteUser)
-
-    / app.get('/tickets', ticketServices.getTicket)
+app.get('/tickets', ticketServices.getTicket)
 app.post('/tickets', ticketServices.postTicket)
 app.put('/tickets/:id', ticketServices.putTicket)
 app.delete('/tickets/:id', ticketServices.deleteTicket)
-
-
 app.listen(port, () => {
-    console.log(`Server on port`, port);
-});
+    console.log(`Server on port`, port)
+})
--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,50 +1,48 @@
-const pool = require('../db_conn/dbconn')
+const pool = require('../database/connection')
 const getUser = async (req: any, res: any) => {
-
     try {
         await pool.query('SELECT * FROM user', (error: any, results: any, fields: any) => {
-            if (error) throw error;
+            if (error) throw error
             res.status(200).send(results)
-        });
+        })
     } catch (error) {
         throw (error)
     }
 }
 const putUser = async (req: any, res: any) => {
-    const id = req.params.id;
+    const id = req.params.id
     const { name, email, cellphone, age, address, country, state } = req.body
     if (!id) {
         throw res.status(400)
     }
     try {
         await pool.query('UPDATE user SET name=?, email=?, cellphone=?, age=?, address=?, country=?, state=? WHERE user_id = ?', [name, email, cellphone, age, address, country, state, id], (error: any, result: any) => {
-            if (error) throw error;
-            res.status(200).send(`User updated successfully with name: ${name}, email:${email}, cellphone:${cellphone}, age=${age}, address:${address}, country:${country}, state:${state}`);
+            if (error) throw error
+            res.status(200).send(`User updated successfully with name: ${name}, email:${email}, cellphone:${cellphone}, age=${age}, address:${address}, country:${country}, state:${state}`)
         })
     } catch (error) {
         throw (error)
     }
-
 }
 const postUser = async (req: any, res: any) => {
     const { name, email, cellphone, age, address, country, state } = req.body
     try {
         await pool.query('INSERT INTO user (name, email, cellphone, age, address, country, state) values(?, ?,?,?,?,?,?)', [name, email, cellphone, age, address, country, state], (error: any, result: any) => {
-            if (error) throw error;
-            res.status(201).send(`User added with ID: ${result.insertId}, name:${name}, email:${email}, cellphone:${cellphone}, age:${age}, address:${address}, country:${country}, state:${state}`);
-        });
+            if (error) throw error
+            res.status(201).send(`User added with ID: ${result.insertId}, name:${name}, email:${email}, cellphone:${cellphone}, age:${age}, address:${address}, country:${country}, state:${state}`)
+        })
     } catch (error) {
         throw (error)
     }
 }
 const deleteUser = async (req: any, res: any) => {
-    const id = req.params.id;
+    const id = req.params.id
     if (!id) {
         throw res.status(400)
     }
     try {
         await pool.query('DELETE FROM user WHERE user_id =?', id, (error: any, result: any) => {
-            if (error) throw error;
+            if (error) throw error
             res.status(200).send(`User wih id ${id} delete sucessfully `)
         })
     } catch (error) {
