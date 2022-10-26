0c3ba47303f0e28b6d0251668a2e489039db5584
sharon calcina <sharon46calcina@gmail.com>
2022-10-25 23:55:28 -0600

    bug: not obtaining response from database

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,58 @@
 
 const express = require('express')
-const conexion = require('mysql')
-
+const mysql = require('mysql')
+import 'dotenv/config'
 // Initializations
 const app = express();
-require('dotenv').config();
-const config = {
+const pool = mysql.createPool({
+    connectionLimit: 5,
     host: 'localhost',
     user: 'root',
-    password: 'root',
-    dataBase: 'apiTest',
-};
-
-const pool = conexion.createPool(config);
-const users: string[] = [];
-const port = process.env.PORT
+    password: 'password',
+    database: 'mundial'
+})
+const port = 4000
 
 // middlewares
 app.use(express.json());
 
-// Routes
-app.get('/users', (req: any, res: any) => {
+const getUser = (req: any, res: any) => {
+    res.send('heloooo')
 
-    const getUsers = async () => {
-        await pool.query('SELECT * FROM useres', (error: any, result: any) => {
+    pool.query('SELECT * FROM user', (error: any, results: any, fields: any) => {
+        // When done with the connection, release it.
+        //connection.release();
+        res.send(results)
+
+        // Handle error after the release.
+        if (error) throw error;
+
+        // Don't use the connection here, it has been returned to the pool.
+    });
+
+
+    /*
+    res.send('holiiiii xd')
+    //try {
+    pool.getConnection((err: any, connection: any) => {
+        if (err) throw err;
+        pool.query('SELECT * FROM mundial.ticket', (error: Error, result: any) => {
+            //res.send(result['rows'])
             if (error) throw error;
-            res.status(200).send(result)
-        });
-    }
+            res.status(200).json("ressss")
+ 
+        })
+    });
+    /* } catch (error) {
+         throw (error)
+     }*/
 
-});
+
+}
+app.get('/users', getUser)
+/*
 app.post('/users', (req: any, res: any) => {
-
+ 
     const sendUsers = async () => {
         await pool.query('INSERT INTO users SET ?', req.body, (error: any, result: any) => {
             if (error) throw error;
@@ -39,14 +60,14 @@ app.post('/users', (req: any, res: any) => {
         });
     }
 });
-
+ 
 app.put('/users/:id', (req: any, res: any) => {
     const id = res.params.id;
     if (!id) {
         throw res.status(400)
     }
     const sendUser = async () => {
-       await pool.query('UPDATE users SET ? WHERE id = ?', [req.body, id], (error: any, result: any) => {
+        await pool.query('UPDATE users SET ? WHERE id = ?', [req.body, id], (error: any, result: any) => {
             if (error) throw error;
             res.status(200).send('User updated successfully.');
         })
@@ -57,13 +78,13 @@ app.delete('/users/:id', (req: any, res: any) => {
     if (!id) {
         throw res.status(400)
     }
-    const deleteUser=async () => {
+    const deleteUser = async () => {
         await pool.query('DELETE FROM users WHERE id =?', id, (error: any, result: any) => {
             if (error) throw error;
             res.status(200).send('User delete sucessfully')
-        }) 
+        })
     }
-});
+});*/
 
 app.listen(port, () => {
     console.log(`Server on port`, port);
