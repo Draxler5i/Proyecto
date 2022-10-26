72214c76e7b1dcbff65ac28d198d3c4d0fa35689
sharon calcina <sharon46calcina@gmail.com>
2022-10-26 05:41:12 -0600

    services for ticket and users done

--- src/index.ts
+++ src/index.ts
@@ -1,90 +1,25 @@
 
-const express = require('express')
-const mysql = require('mysql')
 import 'dotenv/config'
+const express = require('express')
+
 // Initializations
+const userServices = require('./services/userServices')
+const ticketServices = require('./services/ticketServices')
+
 const app = express();
-const pool = mysql.createPool({
-    connectionLimit: 5,
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'mundial'
-})
-const port = 4000
-
-// middlewares
+const port = process.env.PORT || 4005
 app.use(express.json());
+// middlewares
+app.get('/users', userServices.getUser)
+app.post('/users', userServices.postUser)
+app.put('/users/:id', userServices.putUser)
+app.delete('/users/:id', userServices.deleteUser)
 
-const getUser = (req: any, res: any) => {
-    res.send('heloooo')
+    / app.get('/tickets', ticketServices.getTicket)
+app.post('/tickets', ticketServices.postTicket)
+app.put('/tickets/:id', ticketServices.putTicket)
+app.delete('/tickets/:id', ticketServices.deleteTicket)
 
-    pool.query('SELECT * FROM user', (error: any, results: any, fields: any) => {
-        // When done with the connection, release it.
-        //connection.release();
-        res.send(results)
-
-        // Handle error after the release.
-        if (error) throw error;
-
-        // Don't use the connection here, it has been returned to the pool.
-    });
-
-
-    /*
-    res.send('holiiiii xd')
-    //try {
-    pool.getConnection((err: any, connection: any) => {
-        if (err) throw err;
-        pool.query('SELECT * FROM mundial.ticket', (error: Error, result: any) => {
-            //res.send(result['rows'])
-            if (error) throw error;
-            res.status(200).json("ressss")
- 
-        })
-    });
-    /* } catch (error) {
-         throw (error)
-     }*/
-
-
-}
-app.get('/users', getUser)
-/*
-app.post('/users', (req: any, res: any) => {
- 
-    const sendUsers = async () => {
-        await pool.query('INSERT INTO users SET ?', req.body, (error: any, result: any) => {
-            if (error) throw error;
-            res.status(201).send(`User added with ID: ${result.insertId}`);
-        });
-    }
-});
- 
-app.put('/users/:id', (req: any, res: any) => {
-    const id = res.params.id;
-    if (!id) {
-        throw res.status(400)
-    }
-    const sendUser = async () => {
-        await pool.query('UPDATE users SET ? WHERE id = ?', [req.body, id], (error: any, result: any) => {
-            if (error) throw error;
-            res.status(200).send('User updated successfully.');
-        })
-    }
-});
-app.delete('/users/:id', (req: any, res: any) => {
-    const id = res.params.id;
-    if (!id) {
-        throw res.status(400)
-    }
-    const deleteUser = async () => {
-        await pool.query('DELETE FROM users WHERE id =?', id, (error: any, result: any) => {
-            if (error) throw error;
-            res.status(200).send('User delete sucessfully')
-        })
-    }
-});*/
 
 app.listen(port, () => {
     console.log(`Server on port`, port);
---
+++ src/services/ticketServices.ts
@@ -0,0 +1,59 @@
+const ticket = require('../db_conn/dbconn')
+const getTicket = async (req: any, res: any) => {
+
+    try {
+        await ticket.query('SELECT * FROM ticket', (error: any, results: any, fields: any) => {
+            if (error) throw error;
+            res.status(200).send(results)
+        });
+    } catch (error) {
+        throw (error)
+    }
+}
+const putTicket = async (req: any, res: any) => {
+    const id = req.params.id;
+    const { price, category, match_date, stadio } = req.body
+    if (!id) {
+        throw res.status(400)
+    }
+    try {
+        await ticket.query('UPDATE ticket SET price=?, category=?, match_date=?, stadio=? WHERE ticket_id = ?', [price, category, match_date, stadio, id], (error: any, result: any) => {
+            if (error) throw error;
+            res.status(200).send(`Ticket updated successfully with id: ${id}, price: ${price}, category:${category}, match_date:${match_date}, stadio=${stadio}`);
+        })
+    } catch (error) {
+        throw (error)
+    }
+
+}
+const postTicket = async (req: any, res: any) => {
+    const { price, category, match_date, stadio } = req.body
+    try {
+        await ticket.query('INSERT INTO ticket (price, category, match_date, stadio) values(?,?,?,?)', [price, category, match_date, stadio], (error: any, result: any) => {
+            if (error) throw error;
+            res.status(201).send(`Ticket added with ID: ${result.insertId}, price:${price}, category:${category}, match_date:${match_date}, stadio:${stadio}`);
+        });
+    } catch (error) {
+        throw (error)
+    }
+}
+const deleteTicket = async (req: any, res: any) => {
+    const id = req.params.id;
+    if (!id) {
+        throw res.status(400)
+    }
+    try {
+        await ticket.query('DELETE FROM ticket WHERE ticket_id =?', id, (error: any, result: any) => {
+            if (error) throw error;
+            res.status(200).send(`Ticket wih id ${id} deleted sucessfully `)
+        })
+    } catch (error) {
+        throw (error)
+    }
+}
+module.exports = {
+    getTicket,
+    putTicket,
+    postTicket,
+    deleteTicket
+}
