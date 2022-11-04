c75a19106e98c26a4362ad0634b21b224939938e
Anahi <anahicosi@gmail.com>
2022-11-04 16:26:46 -0400

    eslint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,20 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'standard-with-typescript',
-    'eslint-config-prettier'
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    "project": ["./tsconfig.json"]
-  },
-  rules: {
-    "@typescript-eslint/no-misused-promises": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/strict-boolean-expressions": "off",
-    "@typescript-eslint/restrict-template-expressions":"off",
-    "@typescript-eslint/restrict-plus-operands":"off"
-  }
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['standard-with-typescript', 'eslint-config-prettier'],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: ['./tsconfig.json'],
+	},
+	rules: {
+		'@typescript-eslint/no-misused-promises': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/strict-boolean-expressions': 'off',
+		'@typescript-eslint/restrict-template-expressions': 'off',
+		'@typescript-eslint/restrict-plus-operands': 'off',
+	},
 }
