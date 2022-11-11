8c112049dabec10e78ba4f16b15e4c2feb153c6f
sharon calcina <sharon46calcina@gmail.com>
2022-11-11 14:50:55 -0600

    correcting code with good practices

--- src/services/stadium.service.ts
+++ src/services/stadium.service.ts
@@ -4,15 +4,11 @@ const errors = require(`./errorMessages/errors`)
 
 const getStadiums = async (_req: Express.Request, res: Express.Response) => {
     try {
-        await client.query(`SELECT * from stadium`, (error: Error, result: typeof QueryResult) => {
-            if (error) {
-                return res.status(400).send(errors.ERROR_GET)
-            }
-            return res.status(200).json(result.rows)
-        })
+        const stadiums = await client.query(`SELECT * from stadium`)
+        return res.status(200).json(stadiums.rows)
     } catch (e) {
         console.log(e)
-        return res.send(errors.throw_error(e))
+        return res.status(400).send(errors.throw_error(errors.ERROR_GET('stadiums')))
     }
 }
 const createStadium = async (req: Express.Request, res: Express.Response) => {
@@ -22,17 +18,13 @@ const createStadium = async (req: Express.Request, res: Express.Response) => {
         return res.status(400).send(errors.ERROR_VARIABLE)
     }
     try {
-        await client.query(`INSERT INTO public.stadium ( stadium_name, address ) VALUES ($1, $2)`,
-            [stadium_name, address], (error: Error, results: typeof QueryResult) => {
-                if (error) {
-                    return res.status(400).send(ERROR_STADIUM)
-                }
-                return res.status(201).send(`Stadium added with stadium_name: ${stadium_name}, 
-                stadium address: ${address}`)
-            })
+        const stadium = await client.query(`INSERT INTO public.stadium ( stadium_name, address ) 
+        VALUES ($1, $2) RETURNING *`, [stadium_name, address])
+        return res.status(201).send(`Stadium added with stadium_name: ${stadium.stadium_name}, 
+        stadium address: ${stadium.address}`)
     } catch (e) {
         console.log(ERROR_STADIUM)
-        return res.send(errors.throw_error(e))
+        return res.send(errors.throw_error(ERROR_STADIUM))
     }
 }
 const updateStadium = async (req: Express.Request, res: Express.Response) => {
@@ -43,51 +35,33 @@ const updateStadium = async (req: Express.Request, res: Express.Response) => {
         return res.status(400).send(errors.ERROR_VARIABLE)
     }
     try {
-        await client.query(`select * from public.stadium where stadium_id = $1`,
-            [id], async (error: Error, results: typeof QueryResult) => {
-                if (error) {
-                    return res.status(400).send(UPDATE_STADIUM_ERROR)
-                }
-                if (results.rowCount == 0) {
-                    return res.status(400).send(UPDATE_STADIUM_ERROR)
-                }
-                await client.query(`UPDATE stadium SET stadium_name = $1, address = $2 WHERE stadium_id = $3`,
-                    [stadium_name, address, id], (error: Error, results: typeof QueryResult) => {
-                        if (error) {
-                            return res.status(400).send(UPDATE_STADIUM_ERROR)
-                        }
-                        return res.status(200).send(`Stadium modified with stadium name: ${stadium_name}, 
-                address: ${address}`)
-                    })
-            })
+        const stadium = await client.query(`select * from public.stadium where stadium_id = $1`,
+            [id])
+        if (stadium.rowCount == 0) {
+            return res.status(400).send(UPDATE_STADIUM_ERROR)
+        }
+        await client.query(`UPDATE stadium SET stadium_name = $1, address = $2 WHERE stadium_id = $3`,
+            [stadium_name, address, id])
+        return res.status(200).send(`Stadium modified with stadium name: ${stadium_name}, address: ${address}`)
     } catch (e) {
         console.log(e)
-        return res.send(errors.throw_error(e))
+        return res.send(errors.throw_error(UPDATE_STADIUM_ERROR))
     }
 }
 const deleteStadium = async (req: Express.Request, res: Express.Response) => {
     const id = parseInt(req.params.id)
     const DELETE_ERROR = errors.ERROR_MESSAGE('DELETE stadium')
     try {
-        await client.query(`select * from public.stadium where stadium_id = $1`,
-            [id], async (error: Error, results: typeof QueryResult) => {
-                if (error) {
-                    return res.status(400).send(DELETE_ERROR)
-                }
-                if (results.rowCount == 0) {
-                    return res.status(400).send(DELETE_ERROR)
-                }
-                await client.query(`DELETE FROM stadium WHERE stadium_id = $1`, [id],
-                    (error: Error, results: typeof QueryResult) => {
-                        if (error) {
-                            return res.status(400).send(DELETE_ERROR)
-                        }
-                        return res.status(200).send(`Stadium deleted with ID: ${id}`)
-                    })
-            })
+        const stadiums = await client.query(`select * from public.stadium where stadium_id = $1`, [id])
+        if (stadiums.rowCount == 0) {
+            return res.status(400).send(DELETE_ERROR)
+        }
+        await client.query(`DELETE FROM stadium WHERE stadium_id = $1`, [id])
+        return res.status(200).send(`Stadium deleted with ID: ${id}`)
+
     } catch (e) {
         console.log(e)
-        return res.send(errors.throw_error(e))
+        return res.send(errors.throw_error(DELETE_ERROR))
     }
 }
 
