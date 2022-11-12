15678badbbd5faf01252db080a415214b03a2e24
sharon calcina <sharon46calcina@gmail.com>
2022-11-11 18:46:51 -0600

    code corrected with async await and other observations

--- src/routes/ticket.route.ts
+++ src/routes/ticket.route.ts
@@ -6,7 +6,7 @@ const route = Router()
 route
     .get('/', ticketService.getTicket)
     .post('/', ticketService.createTicket)
-    .patch('/:id', ticketService.updateTicket)
+    .put('/:id', ticketService.updateTicket)
     .delete('/:id', ticketService.deleteTicket)
 
 export = route
